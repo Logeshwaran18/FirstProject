6f419678a6b7f1cf4867da8e2671f1a0ab90f400
Logeshwaran18 <logeshwaran199718@gmail.com>
2021-02-15 10:38:54 +0530

    comleted

--- src/test/java/org/login/tester/Example.java
+++ src/test/java/org/login/tester/Example.java
@@ -3,7 +3,6 @@ package org.login.tester;
 public class Example {
 	private void data() {
 		System.out.println("project class");
-
 	}
 	public static void main(String[] args) {
 		Example aa = new Example();
